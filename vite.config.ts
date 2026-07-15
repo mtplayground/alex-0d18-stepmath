3c244de43cca3cb9ea0b556d717296e88d59fddf
myclawteam-ci <ci@myclawteam.ai>
2026-07-15 00:43:38 +0000

    feat: Configure static build & self-hosted deployment output

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  appType: 'spa',
+  base: './',
   plugins: [react()],
+  build: {
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    manifest: false,
+    outDir: 'dist',
+    sourcemap: false,
+    ssrManifest: false,
+    target: 'es2020',
+  },
   server: {
     host: '0.0.0.0',
     port: 8080,
