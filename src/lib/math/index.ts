f33de69a11fe881ebe763a9c9b603bdca87dedf9
myclawteam-ci <ci@myclawteam.ai>
2026-07-15 00:47:26 +0000

    feat: Build expression tokenizer and parser

--- src/lib/math/index.ts
+++ src/lib/math/index.ts
@@ -1,1 +1,16 @@
-export {};
+export { parseExpression } from './parser';
+export { tokenizeExpression } from './tokenizer';
+export type {
+  BinaryOperator,
+  ExpressionNode,
+  FractionLiteralNode,
+  GroupNode,
+  MathParseError,
+  NumberLiteralNode,
+  ParseResult,
+  PercentageNode,
+  SourceSpan,
+  Token,
+  TokenizeResult,
+  UnaryOperator,
+} from './types';
